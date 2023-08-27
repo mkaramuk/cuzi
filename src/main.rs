1ed48bd9cbef8d6c45183a4a92e25eec58ba6712
Yusuf <yusuftaha9@gmail.com>
2023-08-27 19:31:44 +0300

    now works, kinda

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 use std::{fmt, net::SocketAddr, sync::Arc};
 use tokio::{
     fs::File,
-    io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, ReadHalf, WriteHalf},
     net::{TcpListener, TcpStream},
     sync::Mutex,
 };
@@ -35,6 +35,7 @@ async fn main() {
         .await
         .unwrap();
 
+    // Zaten sadece read yapılacak, acaba bunun bi yöntemi yok mu
     let config_arc = Arc::new(Mutex::new(config));
 
     loop {
@@ -48,37 +49,52 @@ async fn read_config() -> Config {
     let mut file = File::open("./config.json").await.unwrap();
     let mut file_content = String::new();
     file.read_to_string(&mut file_content).await.unwrap();
-    file.shutdown().await.unwrap();
-
     serde_json::from_str(file_content.as_str()).unwrap()
 }
 
 async fn handler(
-    mut from: ReadHalf<TcpStream>,
+    mut from: ReadHalfBuf,
     mut to: WriteHalf<TcpStream>,
-    mut buffer: Vec<u8>,
     proxy_info: ProxyConfig,
+    name: &str,
 ) {
     loop {
-        let line_bytes = extract_line(&mut from, &mut buffer).await;
-        let line = String::from_utf8(line_bytes.to_vec());
+        let mut line = String::new();
 
-        if line.is_err() {
-            to.write_all(&line_bytes).await.unwrap();
+        // Returns newline too
+        if from.read_line(&mut line).await.unwrap() == 0 {
+            break;
+        }
+        println!("{name}: {}", line[..line.len() - 2].to_string());
+
+        if line == "\r\n" {
+            to.write_all(b"\r\n").await.unwrap();
+            break;
+        }
+        if line.starts_with("Host:") {
+            to.write_all(format!("Host: {}\r\n", proxy_info.target).as_bytes())
+                .await
+                .unwrap();
         } else {
-            let line = line.unwrap();
-            if line.starts_with("Host:") {
-                to.write_all(format!("Host: {}\r\n", proxy_info.target).as_bytes())
-                    .await
-                    .unwrap();
-            } else {
-                to.write_all(line.as_bytes()).await.unwrap();
-            }
+            to.write_all(line.as_bytes()).await.unwrap();
         }
     }
+
+    loop {
+        let mut buffer = [0; 1024];
+        let read = from.read(&mut buffer).await.unwrap();
+
+        if read == 0 {
+            break;
+        }
+
+        to.write_all(&buffer[..read]).await.unwrap();
+    }
+
+    println!("{name}: EOF");
 }
 
-fn find_proxy(path: String, config: &Config) -> Option<ProxyConfig> {
+fn find_proxy(path: &str, config: &Config) -> Option<ProxyConfig> {
     for proxy in &config.proxies {
         if proxy.path == path {
             return Some(proxy.clone());
@@ -88,72 +104,45 @@ fn find_proxy(path: String, config: &Config) -> Option<ProxyConfig> {
     return None;
 }
 
-fn get_line(data: &[u8]) -> Option<(Vec<u8>, usize)> {
-    if let Some(index) = data.iter().position(|&byte| byte == b'\n') {
-        return Some((data[0..=index].to_vec(), index + 1));
-    }
-    None
-}
+type ReadHalfBuf = BufReader<ReadHalf<TcpStream>>;
 
-fn parse_path(line: &String) -> Option<String> {
-    let path_info: Vec<&str> = line.split(' ').collect();
+async fn find_path(buf: &mut ReadHalfBuf, config: &Config) -> (TcpStream, ProxyConfig) {
+    let mut line = String::new();
 
-    if path_info.len() > 1 {
-        return Some(path_info[1].to_string());
-    }
+    buf.read_line(&mut line).await.unwrap();
 
-    None
-}
+    let splitted: Vec<_> = line.split(' ').collect();
+    let (_protocol, uri, _versionn) = if let [protocol, uri, version] = splitted[..] {
+        (protocol, uri, version)
+    } else {
+        panic!("Invalid request");
+    };
 
-async fn extract_line(reader: &mut ReadHalf<TcpStream>, buffer: &mut Vec<u8>) -> Vec<u8> {
-    let mut temp_buffer = vec![0; 1024];
+    let proxy = find_proxy(uri, config).unwrap();
+    let mut server_stream = TcpStream::connect(proxy.to_string()).await.unwrap();
 
-    loop {
-        if let Some((line, end_index)) = get_line(&buffer) {
-            *buffer = buffer[end_index..].to_vec();
-            return line;
-        }
-        reader.read(&mut temp_buffer).await.unwrap();
-        buffer.extend(temp_buffer.iter());
-    }
-}
+    server_stream.write_all(line.as_bytes()).await.unwrap();
 
-async fn find_path(
-    client: &mut ReadHalf<TcpStream>,
-    config: &Config,
-) -> (TcpStream, ProxyConfig, Vec<u8>) {
-    let mut total_buffer: Vec<u8> = Vec::new();
-    let mut buffer: Vec<u8> = Vec::new();
-
-    loop {
-        let line_bytes = extract_line(client, &mut buffer).await;
-        let line = String::from_utf8(line_bytes.to_vec()).unwrap();
-        total_buffer.extend(line_bytes.iter());
-
-        if line.starts_with("GET") {
-            if let Some(path) = parse_path(&line) {
-                let proxy = find_proxy(path, config).unwrap();
-                let server_stream = TcpStream::connect(proxy.to_string()).await.unwrap();
-                total_buffer.extend(buffer.iter());
-
-                return (server_stream, proxy, total_buffer);
-            }
-        }
-    }
+    return (server_stream, proxy);
 }
 
 async fn handle_client(stream: TcpStream, addr: SocketAddr, config_arc: ArcConfig) {
     println!("Client connected from {}:{}", addr.ip(), addr.port());
 
     let config = config_arc.lock().await;
-    let (mut stream_read, stream_write) = tokio::io::split(stream);
-    let (server_stream, proxy, total_buffer) = find_path(&mut stream_read, &config).await;
+    let (stream_read, stream_write) = tokio::io::split(stream);
+
+    let mut stream_read_buf = BufReader::new(stream_read);
+
+    let (server_stream, proxy) = find_path(&mut stream_read_buf, &config).await;
     let (server_read, server_write) = tokio::io::split(server_stream);
+
+    let server_read_buf = BufReader::new(server_read);
 
     let client_proxy_info = proxy.clone();
 
     tokio::join!(
-        async move { handler(stream_read, server_write, total_buffer, client_proxy_info).await },
-        async move { handler(server_read, stream_write, Vec::new(), proxy).await }
+        async move { handler(stream_read_buf, server_write, client_proxy_info, "<").await },
+        async move { handler(server_read_buf, stream_write, proxy, ">").await }
     );
 }
